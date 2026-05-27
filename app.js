abaf5271635d8e6fe03607136cba93714173d2db
Sachin Panchal <mrsachinpanchal8@gmail.com>
2026-05-27 13:00:24 +0100

    Make Redis optional for AWS deployment

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-require('dotenv').config(); //load env variables
+//load env variables
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 
 const express = require('express');
 const redis = require('redis');
@@ -10,13 +15,27 @@ const MESSAGE = process.env.APP_MESSAGE || "Hello from Platform Lite 🚀";
 
 
 // Redis client setup
-const redisClient = redis.createClient({
-    url: 'redis://redis:6379'  // 👈 service name from docker-compose
-});
+// const redisClient = redis.createClient({
+//     url: 'redis://redis:6379'  // 👈 service name from docker-compose
+// });
 
 
-redisClient.connect().catch(console.error);
+// redisClient.connect().catch(console.error);
 
+let redisClient;
+
+if (process.env.REDIS_HOST) {
+    redisClient = redis.createClient({
+        url: `redis://${process.env.REDIS_HOST}:6379`
+    });
+
+    redisClient.connect()
+        .then(() => console.log('✅ Connected to Redis'))
+        .catch(err => {
+            console.error('❌ Redis connection failed, falling back to app memory', err);
+            redisClient = null;
+        });
+}
 
 // Health check endpoint (super important in real-world apps)
 app.get('/health', (req, res) => {
@@ -25,21 +44,41 @@ app.get('/health', (req, res) => {
 
 
 // Endpoint using Redis
+// app.get('/api/message', async (req, res) => {
+//     try {
+//         let message = await redisClient.get('message');
+
+//         if (!message) {
+//             message = MESSAGE;
+//             await redisClient.set('message', message);
+//         }
+
+//         res.json({ message, source: 'redis-cache' });
+//     } catch (err) {
+//         res.status(500).json({ error: err.message });
+//     }
+// });
+
 app.get('/api/message', async (req, res) => {
     try {
-        let message = await redisClient.get('message');
+        if (redisClient) {
+            let message = await redisClient.get('message');
 
-        if (!message) {
-            message = MESSAGE;
-            await redisClient.set('message', message);
+            if (!message) {
+                message = MESSAGE;
+                await redisClient.set('message', message);
+            }
+
+            return res.json({ message, source: 'redis-cache' });
         }
 
-        res.json({ message, source: 'redis-cache' });
+        // fallback if redis not present
+        res.json({ message: MESSAGE, source: 'app-memory' });
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
